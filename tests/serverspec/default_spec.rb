11cc6e0ca6f8405b33a5b0b0b3cd30dc47f0ab9c
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-02-17 16:23:56 +0900

    fix wrong permissions
    fixes #20

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 require 'serverspec'
 
+default_user    = "root"
+default_group   = "root"
 es_package_name = 'elasticsearch'
 es_service_name = 'elasticsearch'
 es_config_path  = '/etc/elasticsearch'
@@ -9,18 +11,38 @@ es_user_group   = 'elasticsearch'
 
 es_plugin_command = '/usr/share/elasticsearch/bin/plugin'
 es_plugins_directory = '/usr/share/elasticsearch/plugins'
+es_data_directory = "/var/lib/elasticsearch"
+es_log_directory  = "/var/log/elasticsearch"
 
 case os[:family]
 when 'freebsd'
+  default_group = "wheel"
   es_package_name = 'elasticsearch2'
   es_config_path = '/usr/local/etc/elasticsearch'
   es_plugin_command = '/usr/local/bin/elasticsearch-plugin'
   es_plugins_directory = '/usr/local/lib/elasticsearch/plugins'
+  es_data_directory = "/var/db/elasticsearch"
 when "openbsd"
+  default_group = "wheel"
   es_user_name = "_elasticsearch"
   es_user_group = "_elasticsearch"
   es_plugin_command = "/usr/local/elasticsearch/bin/plugin"
   es_plugins_directory = "/usr/local/elasticsearch/plugins"
+  es_data_directory = "/var/elasticsearch"
+end
+
+describe file(es_data_directory) do
+  it { should be_directory }
+  it { should be_owned_by es_user_name }
+  it { should be_grouped_into es_user_group }
+  it { should be_mode 750 }
+end
+
+describe file(es_log_directory) do
+  it { should be_directory }
+  it { should be_owned_by es_user_name }
+  it { should be_grouped_into es_user_group }
+  it { should be_mode 750 }
 end
 
 describe service(es_service_name) do
@@ -33,8 +55,18 @@ end
 
 case os[:family]
 when "freebsd"
+  describe file("/etc/rc.conf.d") do
+    it { should be_directory }
+    it { should be_mode 755 }
+    it { should be_owned_by default_user }
+    it { should be_grouped_into default_group }
+  end
+
   describe file("/etc/rc.conf.d/elasticsearch") do
     it { should be_file }
+    it { should be_mode 644 }
+    it { should be_owned_by default_user }
+    it { should be_grouped_into default_group }
     its(:content) { should match(Regexp.escape('JAVA_OPTS="-XX:+UseCompressedOops"')) }
   end
 
@@ -50,6 +82,9 @@ when "freebsd"
 when "ubuntu"
   describe file("/etc/default/elasticsearch") do
     it { should be_file }
+    it { should be_mode 644 }
+    it { should be_owned_by default_user }
+    it { should be_grouped_into default_group }
     its(:content) { should match(/^ES_JAVA_OPTS=\"#{ Regexp.escape("-XX:+UseCompressedOops") }\"$/) }
   end
 
@@ -60,6 +95,9 @@ when "ubuntu"
 when "redhat"
   describe file("/etc/sysconfig/elasticsearch") do
     it { should be_file }
+    it { should be_mode 644 }
+    it { should be_owned_by default_user }
+    it { should be_grouped_into default_group }
     its(:content) { should match(/^ES_JAVA_OPTS=\"#{ Regexp.escape("-XX:+UseCompressedOops") }\"$/) }
   end
 
@@ -70,6 +108,9 @@ when "redhat"
 when "openbsd"
   describe file("/etc/elasticsearch/jvm.in") do
     it { should be_file }
+    it { should be_mode 644 }
+    it { should be_owned_by default_user }
+    it { should be_grouped_into default_group }
     its(:content) { should match(/JAVA_OPTS=\"#{ Regexp.escape("-XX:+UseCompressedOops") }\"$/) }
   end
 
@@ -89,6 +130,10 @@ end
 end
 
 describe file("#{ es_config_path }/elasticsearch.yml") do
+  it { should be_file }
+  it { should be_owned_by es_user_name }
+  it { should be_grouped_into es_user_group }
+  it { should be_mode 440 }
   its(:content_as_yaml) { should include("cluster.name" => "testcluster") }
   its(:content_as_yaml) { should include("node.name" => "testnode") }
   its(:content_as_yaml) { should include("discovery.zen.ping.multicast.enabled" => "false") }
@@ -100,14 +145,13 @@ describe file("#{ es_config_path }/elasticsearch.yml") do
   its(:content_as_yaml) { should include("http.cors.allow-methods" => "OPTIONS, HEAD, GET, POST, PUT, DELETE") }
   its(:content_as_yaml) { should include("http.cors.allow-headers" => "X-Requested-With, Content-Type, Content-Length") }
   its(:content_as_yaml) { should include("http.cors.allow-credentials" => "true") }
-
-  it { should be_owned_by es_user_name }
-  it { should be_grouped_into es_user_group }
-  it { should be_mode 440 }
 end
 
 describe file(es_plugins_directory) do
   it { should be_directory }
+  it { should be_owned_by es_user_name }
+  it { should be_grouped_into es_user_group }
+  it { should be_mode 755 }
 end
 
 describe command("#{es_plugin_command} list") do
