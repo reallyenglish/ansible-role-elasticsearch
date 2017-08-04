04ea54bf019ed8803eef631b7789924db846c9cd
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-08-05 08:21:45 +0900

    QA

--- tests/integration/three_nodes_cluster/spec/spec_helper.rb
+++ tests/integration/three_nodes_cluster/spec/spec_helper.rb
@@ -1,29 +1,29 @@
-require 'infrataster/rspec'
-require 'infrataster-plugin-firewall'
+require "infrataster/rspec"
+require "infrataster-plugin-firewall"
 
-ENV['VAGRANT_CWD'] = File.dirname(__FILE__)
-ENV['LANG'] = 'C'
+ENV["VAGRANT_CWD"] = File.dirname(__FILE__)
+ENV["LANG"] = "C"
 
 Infrataster::Server.define(
   :es1,
-  '192.168.21.101',
-  vagrant: true,
+  "192.168.21.101",
+  vagrant: true
 )
 
 Infrataster::Server.define(
   :es2,
-  '192.168.21.102',
-  vagrant: true,
+  "192.168.21.102",
+  vagrant: true
 )
 
 Infrataster::Server.define(
   :es3,
-  '192.168.21.103',
-  vagrant: true,
+  "192.168.21.103",
+  vagrant: true
 )
 
 def json_body_as_hash
-    json_str_to_hash(response.body)
+  json_str_to_hash(response.body)
 end
 
 def json_str_to_hash(str)
@@ -34,5 +34,5 @@ RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
 
-  config.order = 'default'
+  config.order = "default"
 end
